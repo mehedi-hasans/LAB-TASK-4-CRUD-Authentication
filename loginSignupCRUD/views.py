7ed51595c10360fdb089dfb42e1f13a4844d7e96
Mehedi Hasan <mehedi4595@gmail.com>
2023-10-29 12:34:10 +0600

    SignUp Authenticate completed

--- loginSignupCRUD/views.py
+++ loginSignupCRUD/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def signupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password')
+        pass2=request.POST.get('confirmpassword')
+
+        if pass1!=pass2:
+            return HttpResponse('Not Match')
+        
+        else:
+            myuser=User.objects.create_user(uname,email,pass1)
+        myuser.save()
+        return redirect('loginPage')
+
     return render(request, 'signup.html')
+
+
+
 
 def loginPage(request):
     return render(request, 'login.html')
